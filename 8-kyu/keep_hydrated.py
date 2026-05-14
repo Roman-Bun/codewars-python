f73992ad421f67bdd4377efb08b5d1a768e40971
Roman Bun <rbun1992@gmail.com>
2026-05-14 11:20:50 +0300

    feat(8kyu): solve Keep Hydrated task

---
+++ 8-kyu/keep_hydrated.py
@@ -0,0 +1,9 @@
+# Task: Keep Hydrated!
+# Link: https://www.codewars.com/kata/582cb0224e56e068d800003c/train/python
+# Level: 8 kyu
+
+def litres(time):
+    return int(time / 2)
+
+print(litres(6.7))
+# litres = 3
