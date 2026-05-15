a6b436c0a6aa6fe0bfe1b28ec74e8fba6b34b45a
Roman Bun <rbun1992@gmail.com>
2026-05-15 10:00:11 +0300

    feat(8kyu): solve Is he gonna survive using multiplication to avoid division by zero

---
+++ 8-kyu/is _he_gonna_survive.py
@@ -0,0 +1,8 @@
+# Task: Is he gonna survive?
+# Link: https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/python
+# Level: 8 kyu
+
+def hero(bullets, dragons):
+    return bullets >= dragons * 2
+
+print(hero(10, 5))
