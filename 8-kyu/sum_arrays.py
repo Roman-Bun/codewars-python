3bae6feaac4dd53e395084314698c245348c535b
Roman Bun <rbun1992@gmail.com>
2026-05-15 10:08:22 +0300

    feat(8kyu): solve Sum Arrays using built-in sum function

---
+++ 8-kyu/sum_arrays.py
@@ -0,0 +1,10 @@
+# Task: Sum Arrays
+# Link: https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
+# Level: 8 kyu
+
+def sum_array(a):
+    return sum(a)
+
+nums = [1, 5.2, 4, 0, -1]
+
+print(sum_array(nums))
