a517c4c31e53a2aa6a5a044074b5f713266d8b3d
Roman Bun <rbun1992@gmail.com>
2026-05-20 13:31:49 +0300

    feat(7kyu): solve Sum of odd numbers using mathematical cube pattern

---
+++ 7-kyu/row_sum_odd_numbers.py
@@ -0,0 +1,8 @@
+# Task: Sum of odd numbers
+# Link: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/python
+# Level: 7 kyu
+
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+print(row_sum_odd_numbers(13))
