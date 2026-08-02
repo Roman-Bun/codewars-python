40634c8cb67f95b40ad0d88bb07ca1f4617a7b78
Roman Bun <rbun1992@gmail.com>
2026-08-02 13:28:55 +0300

    feat(8kyu): solve Returning Strings using f-string interpolation

---
+++ 8-kyu/returning_strings.py
@@ -0,0 +1,9 @@
+# Task: Returning Strings
+# Link: https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+# Level: 8 kyu
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+name = "Roman"
+print(greet(name))
