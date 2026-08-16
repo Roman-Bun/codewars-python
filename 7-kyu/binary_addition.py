6628aa8c3f55466b5bdea7b03aeadb04f02095f3
Roman Bun <rbun1992@gmail.com>
2026-08-16 17:55:54 +0300

    feat(7kyu): solve Binary Addition using f-string binary formatting

---
+++ 7-kyu/binary_addition.py
@@ -0,0 +1,8 @@
+# Task: Binary Addition
+# Link: https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python
+# Level: 7 kyu
+
+def add_binary(a, b):
+    return f"{a + b:b}"
+
+print(add_binary(1, 1))
