010db45bb33b4b96529869f118d726ec4652c698
Roman Bun <rbun1992@gmail.com>
2026-08-19 14:16:09 +0300

    feat(8kyu): solve Cockroach speed using exact unit conversion formula

---
+++ 8-kyu/cockroach.py
@@ -0,0 +1,9 @@
+# Task: Beginner Series #4 Cockroach
+# Link: https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
+# Level: 8 kyu
+
+def cockroach_speed(s):
+    return int(s * 1000 / 36)
+
+speed_kh = 1.08
+print(cockroach_speed(speed_kh))
