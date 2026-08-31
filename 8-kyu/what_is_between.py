863115401c32118305be13b4eb21596c2cbee0f3
Roman Bun <rbun1992@gmail.com>
2026-08-31 13:43:21 +0300

    feat(8kyu): solve What is between using list constructor and range

---
+++ 8-kyu/what_is_between.py
@@ -0,0 +1,8 @@
+# Task: What is between?
+# Link: https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
+# Level: 8 kyu
+
+def between(a, b):
+    return list(range(a, b + 1))
+
+print(between(-2, 2))
